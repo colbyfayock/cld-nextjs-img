ab99c99f79db61590b61930bfb2d126479edb0a8
colbyfayock <fayock@gmail.com>
2022-04-28 12:43:25 -0300

    init

---
+++ src/pages/index.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+import Head from 'next/head'
+
+import Layout from '@components/Layout';
+import Container from '@components/Container';
+import Button from '@components/Button';
+
+import images from '@data/images';
+
+import styles from '@styles/Home.module.scss'
+
+export default function Home() {
+  return (
+    <Layout>
+      <Head>
+        <title>My Images</title>
+        <meta name="description" content="All of my cool images." />
+      </Head>
+
+      <Container>
+        <h1 className="sr-only">My Images</h1>
+
+        <h2 className={styles.header}>Images</h2>
+
+        <ul className={styles.images}>
+          {images.map(image => {
+            return (
+              <li key={image.id}>
+                <a href={image.link} rel="noreferrer">
+                  <div className={styles.imageImage}>
+                    <img width={image.width} height={image.height} src={image.image} alt="" />
+                  </div>
+                  <h3 className={styles.imageTitle}>
+                    { image.title }
+                  </h3>
+                </a>
+              </li>
+            )
+          })}
+        </ul>
+      </Container>
+    </Layout>
+  )
+}
